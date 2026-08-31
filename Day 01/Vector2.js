5874f09d5aedc6708889f6d01e8585cf0c12e8bd
Sentient-Potato <trinhhang@unomaha.edu>
2026-08-31 12:27:08 -0500

    Day 2 stuff

---
+++ Day 01/Vector2.js
@@ -0,0 +1,12 @@
+class Vector2{
+    x
+    y
+
+    //Think of it as __init__ from Python
+    constructor(x, y){
+    //Think of it as self.x from Python
+        this.x = x
+        this.y = y
+    }
+
+}
