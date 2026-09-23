6e6ccb4c7c6ad73dce8f48b5affe10b2ea3f942f
Sentient-Potato <trinhhang@unomaha.edu>
2026-09-23 11:58:59 -0500

    Restructured files to have a 'class' and 'home' folder.

---
+++ Day 01/games/home/Assets.js
@@ -0,0 +1,16 @@
+class Assets {
+    static heart = [
+            new Vector2(0,-5),
+            new Vector2(15,-20),
+            new Vector2(30,10),
+            new Vector2(0,50),
+            new Vector2(-30,10),
+            new Vector2(-15,-20)
+        ]
+    
+    static laser = [
+        new Vector2(0, 0),
+        new Vector2 (20, 25),
+        new Vector2 (-20, 25)
+    ]
+}
---
+++ Day 01/games/home/components/UpdateComponent.js
@@ -0,0 +1,30 @@
+class UpdateComponent extends Component{
+
+    speed = 70
+
+    start(){
+        this.timeSinceLastLaser = 0
+    }
+    update() {
+        //console.log(Input.keysDown) //Debug line, check in console
+        this.timeSinceLastLaser += 1
+
+        if(Input.keysDown.includes("ArrowRight") || Input.keysDown.includes("KeyD"))
+            this.transform.position.x = this.gameObject.transform.position.x + Time.deltaTime * this.speed
+
+        if(Input.keysDown.includes("ArrowLeft") || Input.keysDown.includes("KeyA"))
+            this.transform.position.x = this.gameObject.transform.position.x - Time.deltaTime * this.speed
+        
+        if(Input.keysDown.includes("ArrowUp") || Input.keysDown.includes("KeyW"))
+            this.transform.position.y = this.gameObject.transform.position.y - Time.deltaTime * this.speed
+
+        if(Input.keysDown.includes("ArrowDown") || Input.keysDown.includes("KeyS"))
+            this.transform.position.y = this.gameObject.transform.position.y + Time.deltaTime*  this.speed
+
+        if(this.timeSinceLastLaser > 30){
+            this.timeSinceLastLaser = 0
+            instantiate(new LaserGameObject(), this.transform.position.clone())
+        }
+
+    }
+}
